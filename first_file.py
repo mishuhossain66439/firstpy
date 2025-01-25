0c74e3a84a407ace2c11a045c463fc04d3658704
Md.Mozammel Hossain Khan/MF/BRAC <275716@brac.net>
2025-01-25 21:40:50 +0600

    first commit

---
+++ first_file.py
@@ -0,0 +1,498 @@
+# a = 30
+# b = 20
+
+# print(10 + 20)
+# print(10 - 20)
+# print(10 * 20)
+
+# print(a + b)
+# print(a - b)
+# print(a * b)
+
+# a = 01
+
+# print(a)
+# print(type(a))
+
+# print(6 & 3)
+
+# a = 'Bangladesh'
+# b = str(20)
+# b = '20'
+# b = 20
+
+# print(a + str(b))
+
+# print(type(a))
+
+# a = int(input('Enter A Number :'))
+# b = int(input('Enter 2nd Number :'))
+
+# a = input('Enter A Number :')
+# b = input('Enter 2nd Number :')
+
+# # print(int(a) + int(b))
+# print(int(a) + float(b))
+
+
+a = 'I Love Bangladesh'
+# a = 'i love bangladesh'
+
+# print(a[-17:])
+
+# print(a[::-1])
+
+# print(a.upper())
+# print(a.lower())
+# print(a.capitalize())
+# print(a.title())
+# print(a.find('e'))
+# print(a.replace('bangladesh','india'))
+# print(a.swapcase())
+
+# print(len(a))
+
+# print(a[:-11:-1])
+
+# print(a.index('B'))
+# print(a.strip('.'))
+# print(a.split())
+
+# b = a.split()
+
+# b.insert(2, 'very Much')
+
+# c = ' '.join(b)
+
+# print(c)
+
+# b = a.strip()
+# c = ''.join(b.split())
+
+# print(len(c))
+# c = a.split()
+# b = ''.join(c)
+# print(b)
+
+
+
+
+# b = input('Insert a User Name : ')
+# c = input('Insert a User Age : ')
+# d = input('Insert a Subject : ')
+
+# b= 20
+# c = 45
+# d = 79
+
+# # print(a.format(b,c,d))
+# a = f''' Username : {b}
+#         Age : {c}
+        # Subject :  {d} '''
+# print(a)
+
+# print(f'I {d} love Bangladesh {b} {b + c}')
+
+# a = 'I Love Bangladesh'
+
+# print(a[::-1])
+
+# Artthmet
+# +-*/ % ** //
+# == != < > >= <=
+# and or not
+
+a = 10
+b = 10
+c = 10
+
+
+# a+=3
+# print(a<=b and b>c)
+# print(a<=b or b>c)
+# print(not(a<=b or b>c))
+
+# a = 'I Love Bangledesh'
+# print('l' in a)
+# print('l' not in a)
+   
+# 0 = 0 0 0 0
+# 1 = 0 0 0 1
+# 2 = 0 0 1 0
+# 3 = 0 0 1 1
+# 4 = 0 1 0 0
+# 5 = 0 1 0 1
+# 6 = 0 1 1 0
+# 7 = 0 1 1 1
+
+# & | ^ ~ << >>
+
+# a = 6
+# b = 3
+# c = 4
+
+# print(a ^ b ^ c)
+
+# 6 = 0 1 1 0
+# 3 = 0 0 1 1
+# 4 = 0 1 0 0 
+# 1 = 0 0 0 1
+
+a = 20
+b = 20
+
+# if a > b :
+#    print('a is greater then b')
+# elif a < b:
+#    print('a is smaller then b')   
+# else:
+#    print('invalid')
+   
+# if a < b :
+#     if a == b:
+#         print('')    
+
+# score = int(input('Enter A Score:'))
+
+# if score <= 100 and score >= 0:
+#     if score >= 80 or score > 78:
+#         if score == 79:
+#             score += 1
+#             print(f"A+ (score)")
+#     elif score >= 70:
+#         print('A') 
+#     elif score >= 60:
+#         print('A-')
+# else:
+#     print('Invalid result')
+
+# loop two type while and for
+# a = 10
+# while a == 10:
+#     print('Bangladesh')
+# a = 10
+# while a < 20:
+#     print(a)
+#     a +=1
+
+# while must true    
+
+# a=1
+
+# while a <= 10:
+#     print(a)
+#     if a== 5:
+#         break    
+#     a+=1
+
+# while a < 10:
+#       a+=1
+#       if a == 6 or a == 8:
+#         continue
+#       print(a)    
+  
+# a = 20
+
+# for i in range(1, a+1):
+#     print(i)
+
+# a = 'I love Bangladesh'
+
+# for i in a:
+#     print(i, end='')
+
+# for i in a:
+#     if i == 'B':
+#         break
+#     print(i, end='')
+
+# for i in a:
+#     if i == 'B':
+#         continue
+#     print(i, end='')
+
+#  user taken input  the create number;
+
+# a = int(input('Enter a number:'))
+# b = 1
+# while b <= 10:
+#     print(f'{a} x {b}= {a * b}')
+#     b+= 1
+    
+    
+
+
+# for i in range(1, 11, 2):
+#     if i == 2:
+#         print(a,end="")
+
+# a = [12,14,3, 'bangledesh', True]
+
+# a.append('india')
+# print(a)
+
+# print(a.index('bangledesh'))
+# a.insert(2,'india')
+
+# b = a.copy()
+# print(a)
+# b.insert(2,'india')
+# print(b)
+# print(a.count(12))
+
+# a.remove('bangledesh')
+# a.pop(1)
+
+# print(a)
+
+# a =[12,23,45,4,8,2]
+
+# a.sort(reverse=True)
+
+# a.reverse()
+
+# a.clear()
+
+# print(a)
+
+# for i in a:
+    # print(i)
+
+# # a=[]  
+# # for i in range(1,11):
+#     a.append(i)  
+# print(a)    
+# while True:
+#  print('Type "A" or "a" Addition')
+#  print('Type "B" or "b" Sub')
+#  print('Type "C" or "c" Mult')
+#  print('Type "D" or "d" div')
+#  print('Type "E" or "e" exit')
+#  print('=====================')
+ 
+
+#  operator = input('Enter A operator: ').lower()
+#  num1 = int(input('Enter first Number: '))
+#  num2 = int(input('Enter Second Number: '))
+#  cal=num1+num2
+#  Result=[]
+#  if operator == 'a':
+#   cal={num1+num2}
+#   print(f'{num1}+{num2}={num1+num2}')
+#   Result.append(cal)
+#   print('=========================')
+#  elif operator == 'b':
+#   cal={num1-num2}
+#   print(f'{num1}-{num2}={cal}')
+#   Result.append(cal)
+#   print('=========================')
+#  elif operator == 'e':
+#    break
+#  else:
+#   print('Invalid Number')
+#   print('=========================')
+
+# print(Result)   
+   
+ 
+# a=(12,12, 14, 'Bangladesh')
+
+# print(type(a))
+
+# print(a.count(12))
+
+# print(a.index(14))
+
+# b=list(a)
+# b.append('india')
+# a=tuple(b)
+# print(b)
+# print(a)
+# a = [12, 12, 4, 'Bangladesh']
+
+# for i in a:
+#     # print(i)
+#     if i == 'Bangledesh':
+#     #    b= a.index(i)
+#     #    c=a[b]
+#        a.insert(b,i.upper())
+#        a.pop(b+1)
+#     # print(i)    
+
+# print(a)    
+
+# a = {'name':'Rahim', 'age': 32 , 'Salary': 13500}
+
+# print(a.keys())
+# print(a.values())
+# print(a.get('name'))
+
+# a.update({'address':'Dhaka-1217' })
+
+# for i in a.values():
+#     print(i)
+
+# for i, j in a.items():
+#     print(i,j)
+
+# a.clear()
+# print(a)
+
+# b= a.copy()
+# print(b)
+
+# a.pop('Salary')
+# print(a)
+# c = a.setdefault('Salary')
+# print(c)
+# a = {'Rahim',32,13500}
+
+# # print(type(a))
+# a.add('jjj')
+
+# # print(a)
+
+# for i in a:
+#     print(i)
+
+# def my_funct(*name):
+#     # print(f'Hello Mr. {name[1]}')
+#     a=0
+#     for i in name:
+#         a += i
+#     print(i)
+
+# # a = input('Enter First Name: ')    
+# # b = input('Enter Second Name: ')    
+# # c = input('Enter Third Name: ')    
+# a = int(input('Enter First Name: '))  
+# b = int(input('Enter Second Name: '))
+# f = int(input('Enter Third Name: '))   
+# d = int(input('Enter Third Name: '))   
+# r = int(input('Enter Third Name: '))   
+
+# my_funct(a,b,f,d,r)    
+
+# a=23
+# b=47
+
+# c ='a is bigger then b' if a>b else 'b is bigger then a' 
+
+# print(c)
+
+# c = [i for i in range(10)]
+# c = {i for i in range(1,10)}
+
+# print(c)
+
+# c = lambda num1, num2: num1+ num2
+# print(c(12,12))
+
+# def my_fuc(x):
+#     if x==0 or x==1:
+#         return 1
+#     return x* my_fuc(x-1)   
+
+# print(my_fuc(5))
+
+# def countdown(n):
+#     if n==0:
+#         pass
+#     else:
+#         print(n)
+#         countdown(n-1)
+
+# countdown(10)
+
+# def calculator():
+# class my_class:
+#     a = 10
+#     def my_fun(self,c,d):
+#         print(c+d)
+
+# x = my_class()
+
+# x.my_fun(12,12)
+
+# class father:
+#     a=10
+#     b=20
+# class child(father):
+#     c=90
+#     d=20
+# x= child()
+
+# print(x.a)
+
+# class car:
+#     def move(self):
+#         print('drive')
+        
+# class boat:
+#     def move(self):
+#         print('Sail')
+        
+# class plain:
+#     def move(self):
+#         print('fly')
+
+# a= car()
+# b=boat()
+# c=plain()
+
+# for i in (a,b,c):
+#     i.move()
+
+# class a:
+#     def add(self, *args):
+#         return sum(args)
+# class b:
+#     def add(self, *args):
+#         return sum(args)
+# class c:
+#     def add(self, *args):
+#         self.num= []
+#         for i in range(1,11):
+#             self.num.append(i)
+#         return self.num    
+    
+
+# x= a()
+# y= b()
+# z=c()
+
+# print(x.add(12,22))
+# print(y.add(12,22))
+# print(z.add(12,22))
+
+# inhertan
+# polymaris
+
+# class father:
+#     a=10
+#     b=20
+# class mother:
+#     a=10
+#     b=20
+
+# class child(father,mother):
+#     c=90
+#     d=20
+# x= child()
+
+# print(x.a)
+
+# class filehandle:
+#     def __init__(self, file_name,mode):
+#         self.file= open(file_name,mode)
+#         print(f'{file_name} {mode}')
+#     def write_data(self, data):
+#         self.data.write(data)
+#         print(f'data write successfully')
+#     def __del__(self):
+#         self.file.close()
+#         print('file closed')
+
+# filehandle=filehandle('My_file.txt', 'w')
+# filehandle.write_data('i Love bangladesh')
+
